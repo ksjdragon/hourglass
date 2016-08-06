8e153057182a914883d9b12ee563784de0823cf7
Kenneth Jao <ksjdragon@gmail.com>
2016-08-06 13:02:00 -0400

    Main layout for website

--- client/main.js
+++ client/main.js
@@ -3,20 +3,76 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+var openValues = {
+	"menu": "-25%",
+	"options": "-20%"
+};
+
+var themeColors = {
+	"light": {
+		"header":"#EBEBEB",
+		"statusIcons":"#33ADFF",
+		"sidebar":"tbd"
+	},
+	"dark": {
+
+	}
+}
+
+Session.set("menuOpen", false);
+Session.set("optionsOpen", false);
+
+Cookie.set("theme","light",{'years':15});
+
+Template.header.helpers({
+	headerColor() {
+		return themeColors[Cookie.get("theme")].header;
+	},
+	menuIconColor() {
+		let status = Session.get("menuOpen");
+		if(status) {
+			return themeColors[Cookie.get("theme")].statusIcons;
+		} else {
+			return;
+		}
+	},
+	optionsIconColor() {
+		let status = Session.get("optionsOpen");
+		if(status) {
+			return themeColors[Cookie.get("theme")].statusIcons;
+		} else {
+			return;
+		}
+	}
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.menu.helpers({
+	menuStatus() {
+  		let status = Session.get("menuOpen");
+	  	if(status) {
+	  		return "0%";
+	  	} else {
+	  		return openValues["menu"];
+	  	}
+	}
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.options.helpers({
+	optionsStatus() {
+	  	let status = Session.get("optionsOpen");
+	  	if(status) {
+	  		return "0%";
+	  	} else {
+	  		return openValues["options"];
+	  	}
+	}
 });
+
+Template.header.events({
+	'click .fa-bars' () {
+		Session.set("menuOpen",!Session.get("menuOpen"));
+	},
+	'click .fa-cog' () {
+		Session.set("optionsOpen",!Session.get("optionsOpen"));
+	}
+})
