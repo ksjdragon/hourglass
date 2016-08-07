c7b963f87f769c6238931241827c623edbc2ac2c
Kenneth Jao <ksjdragon@gmail.com>
2016-08-07 15:03:24 -0400

    Added schemas for collections

--- server/main.js
+++ server/main.js
@@ -1,6 +1,42 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
+schools = new Mongo.Collection("Schools");
+classes = new Mongo.Collection("Classes");
+homework = new Mongo.Collection("Homework");
+
+schools.schema = new SimpleSchema({
+	name: {type: String},
+	aliases: {type: [String]}
+});
+
+classes.schema = new SimpleSchema({
+	school: {type: String},
+	//icon: {type: String},
+	hour: {type: String},
+	teacher: {type: String},
+	name: {type: String, label: "Class Name"},
+	status: {type: boolean, defaultValue: false},
+	code: {type: String, optional: true},
+	privacy: {type: String},
+	category: {type: String},
+	/*moderators: {type: [String]},
+	banned: {type: [String]},
+	blockEdit: {type: [String]},
+	admin: {type: String}*/
+});
+
+homework.schema = new SimpleSchema({
+	class: {type: String},
+	dueDate: {type: Date},
+	aliases: {type: [String]},
+	submittor: {type: String},
+	confirmations: {type: [String]},
+	reports: {type: [String], optional: true},
+	attachments: {type: [String], optional: true},
+	done: {type: [String], optional: true}
+});
+
 Meteor.startup(() => {
   Meteor.methods({
 
