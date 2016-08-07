4354507b0e18b777a098b6fc0d131adb73dea827
yamanq <ybq987@gmail.com>
2016-08-07 15:36:47 -0400

    add router

---
+++ client/router.js
@@ -0,0 +1,15 @@
+ Router.route('/', function() {
+  if (Meteor.user() != null) {
+    this.render("main");
+  } else {
+    this.redirect('/login');
+  }
+})
+
+Router.route('/login', function() {
+  if (Meteor.user() == null) {
+    this.render("login");
+  } else {
+    this.redirect('')
+  }
+}); 
