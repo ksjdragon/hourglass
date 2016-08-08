f4294318e72a481dabf8cfde8e3a4196bbcd8bd0
yamanq <ybq987@gmail.com>
2016-08-08 19:34:30 -0400

    add 404 and profile routing

--- client/router.js
+++ client/router.js
@@ -4,12 +4,26 @@
   } else {
     this.redirect('/login');
   }
-})
+});
 
 Router.route('/login', function() {
   if (Meteor.user() == null) {
     this.render("login");
+  } else if (Object.keys(Meteor.user().profile).length <= 1) {
+    this.redirect('/create')
   } else {
     this.redirect('/')
   }
-}); 
+});
+
+Router.route('/profile', function() {
+  if (Meteor.user() != null) {
+    this.render("profile");
+  } else {
+    this.redirect("/login");
+  }
+});
+
+Router.configure({
+  notFoundTemplate: "404"
+});
