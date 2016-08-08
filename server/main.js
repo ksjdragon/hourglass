1a7068c753f96c443dd02339c1ab325c0bcc2747
yamanq <ybq987@gmail.com>
2016-08-08 07:08:40 -0400

    fixed collections bug

--- server/main.js
+++ server/main.js
@@ -1,42 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-schools = new Mongo.Collection("Schools");
-classes = new Mongo.Collection("Classes");
-work = new Mongo.Collection("Work");
-
-schools.schema = new SimpleSchema({
-	name: {type: String},
-	aliases: {type: [String]}
-});
-
-classes.schema = new SimpleSchema({
-	school: {type: String},
-	//icon: {type: String},
-	name: {type: String, label: "Class Name"},
-	hour: {type: String, optional: true},
-	teacher: {type: String, optional: true},
-	admin: {type: String, optional: true},
-	status: {type: Boolean, defaultValue: false},
-	code: {type: String, optional: true},
-	privacy: {type: String},
-	category: {type: String},
-	moderators: {type: [String], optional: true},
-	banned: {type: [String], optional: true},
-	blockEdit: {type: [String], optional: true}
-});
-
-work.schema = new SimpleSchema({
-	class: {type: String},
-	dueDate: {type: Date},
-	aliases: {type: [String]},
-	submittor: {type: String},
-	confirmations: {type: [String]},
-	reports: {type: [String], optional: true},
-	attachments: {type: [String], optional: true},
-	done: {type: [String], optional: true}
-});
-
 _uuid4 = function(cc) {
     var rr = Math.random() * 16 | 0; 
     return (cc === 'x' ? rr : (rr & 0x3 | 0x8)).toString(16);
