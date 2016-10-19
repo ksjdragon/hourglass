59899e357858642916d20d4ac1463c95a27213f4
Kenneth Jao <ksjdragon@gmail.com>
2016-10-19 01:26:24 -0400

    removed unnecessary code

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -588,13 +588,6 @@ Template.profile.events({
                 Session.set("autocompleteDivs", divs);
             }
         } catch (err) {}
-    },
-    'mouseenter .optionText' (event) {
-        console.log("hi");
-        event.target.className += " selectedOption";
-    },
-    'mouseleave .optionText' (event) {
-        event.target.className.replace(" selectedOption", "");
     }
 });
 
