9f1d2593351209a3d8e94ddf4fe8f8b4afbabfff
Kenneth Jao <ksjdragon@gmail.com>
2016-11-03 23:12:57 -0400

    Manage class menu start layout

--- client/menus/menus.js
+++ client/menus/menus.js
@@ -23,7 +23,10 @@ Template.sidebarMenuPlate.helpers({
             });
         }
         return array;
-    }
+    },
+    filterOn() {
+        return Session.get("classDisp").length !== 0 || Session.get("typeFilter").length !== 0;
+    },
 });
 
 Template.sidebarMenuPlate.events({
@@ -62,6 +65,80 @@ Template.sidebarMenuPlate.events({
             $("#classListHolder").slideUp(300);
         }
         filterOpen[2] = !filterOpen[2];
+    },
+    // CLASS FILTERS
+    'click .sideClass' (event) { // Click class list in sidebar.
+        var div = event.target;
+        while (div.getAttribute("classid") === null) div = div.parentNode;
+        var classid = div.getAttribute("classid");
+
+        if (Session.equals("sidebarMode","create")) { // If creating work from calendar.
+            var newSetting = Session.get("currentWork");
+            newSetting.class = classid;
+            Session.set("currentWork", newSetting);
+            openDivFade(document.getElementsByClassName("overlay")[0]);
+        } else { // Normal clicking turns on filter.
+            var array = Session.get("classDisp");
+            if (array.indexOf(classid) !== -1) {
+                array.splice(array.indexOf(classid), 1);
+            } else {
+                array.push(classid);
+            }
+            Session.set("classDisp", array);
+        }
+    },
+    'click .sideFilter' (event) {
+        var div = event.target;
+        while (div.getAttribute("type") === null) div = div.parentNode;
+        var type = div.getAttribute("type");
+
+        var array = Session.get("typeFilter");
+        if (array.indexOf(type) !== -1) {
+            array.splice(array.indexOf(type), 1);
+        } else {
+            array.push(type);
+        }
+        Session.set("typeFilter", array);
+    },
+    'click #disableFilter' () {
+        Session.set("classDisp", []);
+        Session.set("typeFilter", []);
+    },
+    'mouseover .sideClass' (event) { // Highlight/scale filter on-hover.
+        var div;
+        if (event.target.className !== "sideClass") {
+            div = event.target.parentNode;
+        } else {
+            div = event.target;
+        }
+        while (div.getAttribute("classid") === null) div = div.parentNode;
+        var classid = div.getAttribute("classid");
+        Session.set("classDispHover", classid);
+    },
+    'mouseleave .sideClass' (event) { // Turn off highlight/scale filter on-leave.
+        if (event.target.className !== "sideClass") {
+            var div = event.target.parentNode;
+            if (div.contains(event.target)) return;
+        }
+        Session.set("classDispHover", null);
+    },
+    'mouseover .sideFilter' (event) {
+        var div;
+        if (event.target.className !== "sideFilter") {
+            div = event.target.parentNode;
+        } else {
+            div = event.target;
+        }
+        while (div.getAttribute("type") === null) div = div.parentNode;
+        var type = div.getAttribute("type");
+        Session.set("typeFilterHover", type);
+    },
+    'mouseleave .sideFilter' (event) {
+        if (event.target.className !== "sideFilter") {
+            var div = event.target.parentNode;
+            if (div.contains(event.target)) return;
+        }
+        Session.set("typeFilterHover", null);
     }
 });
 
